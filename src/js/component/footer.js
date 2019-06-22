af4a761258e8f200182f68c1e8048aa10c654447
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-06-22 17:09:07 +0000

    skeleton

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<nav className="navbar navbar-light bg-light mb-3">
+			<div className="container btn-group" role="group" aria-label="Basic example">
+				<Link to="/">
+					<button type="button" className="btn btn-secondary">
+						Buscados
+					</button>
+				</Link>
+				<Link to="/">
+					<button type="button" className="btn btn-secondary">
+						Historia
+					</button>
+				</Link>
+				<Link to="/">
+					<button type="button" className="btn btn-secondary">
+						Encontrados
+					</button>
+				</Link>
+			</div>
+		</nav>
 	</footer>
 );
