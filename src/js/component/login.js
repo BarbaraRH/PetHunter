9cdb8fd45d1724193e53a4266c027fbd3265d56a
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-06-24 22:54:24 +0000

    the login seems more neat

--- src/js/component/login.js
+++ src/js/component/login.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
+import logo from "../../img/logo.png";
 
 export const Login = () => (
 	<div className="container">
 		<div className="d-flex justify-content-center h-100">
 			<div className="card">
-				<div className="card-header">
-					<div className="d-flex justify-content-end social_icon" />
-				</div>
 				<div className="card-body">
+					<img src={logo} alt="Pet Hunter" className="logo" />
 					<form>
 						<div className="input-group form-group">
 							<div className="input-group-prepend" />
-							<input type="text" className="form-control" placeholder="username" />
+							<input type="text" className="form-control" placeholder="Pet Lover User" />
 						</div>
 
 						<div className="input-group form-group">
-							<input type="password" className="form-control" placeholder="password" />
+							<input type="Your Password" className="form-control" placeholder="password" />
 						</div>
 
 						<div className="form-group container">
-							<input type="submit" value="Login" className="btn login_btn" />
+							<input type="submit" value="start Searching" className="btn btn-danger" />
 						</div>
 					</form>
 				</div>
--- src/js/layout.js
+++ src/js/layout.js
@@ -45,7 +45,6 @@ export class Layout extends React.Component {
 							<Route path="/petHunterEvaluation" component={PetHunterEvaluation} />
 							<Route render={() => <h1>Not found!</h1>} />
 						</Switch>
-						<Footer />
 					</ScrollToTop>
 				</BrowserRouter>
 			</div>
