361741a7af6185b00fa0897420fcfcd54ea68e80
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-07-24 20:53:17 +0000

    trying to make a preloader

--- src/js/component/loading.js
+++ src/js/component/loading.js
@@ -5,27 +5,13 @@ import cat from "../../img/cat_1.png";
 /* import "./petStyle.css"; */
 
 export const Loading = () => (
-	<div className="bg">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" />
-		<div className="container d-flex">
-			<div className="row w-100 pt-5 mt-5">
-				<div className="col-2" />
-				<div className="col-8 mt-5 pt-5">
-					<div className="cat">
-						<div className="row w-100">
-							<div className="col-2" />
-							<div className="col-8">
-								<div className="loadingAnimal" style={{ height: "200px" }} />
-							</div>
-							<div className="col-2" />
-						</div>
-					</div>
-					<div className="col-2" />
-				</div>
+	<div className="cat">
+		<div className="row w-100">
+			<div className="col-2" />
+			<div className="col-8">
+				<div className="loadingAnimal" style={{ height: "200px" }} />
 			</div>
+			<div className="col-2" />
 		</div>
 	</div>
 );
--- src/js/component/login.js
+++ src/js/component/login.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./petStyle.css";
 import logo from "../../img/logo.png";
+import { Loading } from "./loading";
+import { Login2 } from "./login2";
 
 export const Login = () => (
 	<div className="bg">
@@ -9,42 +11,9 @@ export const Login = () => (
 			<div className="col-2" />
 			<div className="col-8">
 				<div className="card-body mt-5">
-					<div className="container">
-						<div className="row">
-							<div className="col-2" />
-							<div className="col-8">
-								<div alt="Pet Hunter" className="logo" />
-							</div>
-							<div className="col-2" />
-						</div>
-
-						<form>
-							<div className="form-group">
-								<label className="sr-only" htmlFor="user">
-									User
-								</label>
-								<input id="user" type="text" className="form-control" placeholder="Pet Lover User" />
-							</div>
-							<div className="form-group">
-								<label className="sr-only" htmlFor="password">
-									Password
-								</label>
-								<input id="password" type="password" className="form-control" placeholder="password" />
-							</div>
-
-							<Link to={"/lostPets"}>
-								<button type="submit" className="btn">
-									Start Searching
-								</button>
-							</Link>
-							<br />
-							<br />
-							<Link to={"/RegisterForm"}>
-								<button type="submit" className="btn">
-									Register
-								</button>
-							</Link>
-						</form>
+					<div className="container loadingRemover">
+						<Loading />
+						<Login2 />
 					</div>
 				</div>
 			</div>
