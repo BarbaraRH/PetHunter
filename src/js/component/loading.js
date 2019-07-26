4be40a2eb7e1922633da74ae2a0c4c45e70a6bb9
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-07-26 20:12:43 +0000

    the loader works

--- src/js/component/loading.js
+++ src/js/component/loading.js
@@ -5,11 +5,11 @@ import cat from "../../img/cat_1.png";
 /* import "./petStyle.css"; */
 
 export const Loading = () => (
-	<div className="preloader h-100 position-absolute indexpluss">
+	<div className="preloader h-100 w-100 position-absolute indexpluss loading">
 		<div className="cat indexpluss">
 			<div className="row w-100 indexpluss">
 				<div className="col-2 indexpluss" />
-				<div className="col-8 indexpluss">
+				<div className="col-8 indexpluss mt-5 pt-5">
 					<div className="loadingAnimal indexpluss" style={{ height: "200px" }} />
 				</div>
 				<div className="col-2" />
