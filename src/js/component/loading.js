dd176bf671d314ed58ad2d5a14ed76f0e17151a0
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-07-27 14:19:47 +0000

    preloader finished

--- src/js/component/loading.js
+++ src/js/component/loading.js
@@ -7,25 +7,23 @@ import logo2 from "../../img/logo2.png";
 
 export const Loading = () => (
 	<div className="preloader h-100 w-100 position-absolute indexpluss loading">
-		<div className="row">
-			<div className="cat indexpluss">
-				<div className="row w-100 indexpluss">
-					<div className="col-2 indexpluss" />
-					<div className="col-8 indexpluss mt-5 pt-5">
-						<div className="loadingAnimal indexpluss" style={{ height: "200px" }} />
+		<div className="cat indexpluss">
+			<div className="row w-100 indexpluss">
+				<div className="col-2 indexpluss" />
+				<div className="col-8 indexpluss mt-5 pt-5">
+					<div className="row float-left">
+						<div className="col-12" style={{ width: "300px" }}>
+							<div className="loadingAnimal indexpluss" style={{ height: "200px", width: "100%" }} />
+						</div>
 					</div>
-					<div className="col-2" />
+					<div className="row float-left">
+						<div className="col-12" style={{ top: "400px" }}>
+							<img className="img-fluid d-block mx-auto pl-5" src={logo2} />
+						</div>
+					</div>
 				</div>
+				<div className="col-2" />
 			</div>
-		</div>
-		<div className="row mt-5">
-			<div className="col-2" />
-			<div className="col-8">
-				<div>
-					<img className="img-fluid mt-5" src={logo2} />
-				</div>
-			</div>
-			<div className="col-2" />
 		</div>
 	</div>
 );
