8743d434493eebd72afc641ec4076a15579eb2fd
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-07-27 18:37:58 +0000

    a comit mutherfoker

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -10,14 +10,14 @@ export const Footer = () => (
 			width: "100%",
 			zIndex: 1
 		}}>
-		<div className="row p-3 text-center footer" style={{ backgroundImage: "linear-gradient(#d5ffff,#a1ffce)" }}>
-			<Link to="/LostPets" className="col-sm-4 col-4" style={{ color: "rgb(206, 147, 216)" }}>
+		<div className="row p-3 text-center" style={{ backgroundImage: "linear-gradient(#d5ffff,#a1ffce)" }}>
+			<Link to="/LostPets" className="col-sm-4 col-4" style={{ color: "#81b9bf" }}>
 				<i className="fas fa-search fa-2x blanco" />
 			</Link>
-			<Link to="/FindedPets" className="col-sm-4 col-4 blanco" style={{ color: "rgb(206, 147, 216)" }}>
+			<Link to="/FindedPets" className="col-sm-4 col-4 blanco" style={{ color: "#81b9bf" }}>
 				<i className="fas fa-bullseye fa-2x" />
 			</Link>
-			<Link to="/petHunterHistory" className="col-sm-4 col-4 blanco" style={{ color: "rgb(206, 147, 216)" }}>
+			<Link to="/petHunterHistory" className="col-sm-4 col-4 blanco" style={{ color: "#81b9bf" }}>
 				<i className="fas fa-award fa-2x" />
 			</Link>
 		</div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,10 +5,10 @@ import PropTypes from "prop-types";
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="fixed-top container-fluid" style={{ backgroundImage: "linear-gradient(#d5ffff, #a1ffce)" }}>
+			<nav className="fixed-top container-fluid" style={{ backgroundColor: "#b2ebf2" }}>
 				<div className="row">
 					<Link to="/myAccount" className="col-2">
-						<i className="fas fa-exclamation-circle fa-2x mt-2 mb-2" style={{ color: "#ce93d8" }} />
+						<i className="fas fa-exclamation-circle fa-2x mt-2 mb-2" style={{ color: "#d50000" }} />
 					</Link>
 					<h3 className="col-9 text-center mt-2 mb-2">{this.props.title}</h3>
 				</div>
