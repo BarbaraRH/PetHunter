0aacfba3020edc8ec8b30c9e6438dcd76a631948
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-08-01 23:24:06 +0000

    Addform with buttons

--- src/js/views/addForm.js
+++ src/js/views/addForm.js
@@ -11,12 +11,6 @@ export class AddForm extends React.Component {
 			<div>
 				<Navbar />
 				<div clasName="container my-5">
-					<div className="row p-3 mt-4">
-						<div className="col-md-12 text-center p-2">
-							<h2>Datos de mascota</h2>
-						</div>
-					</div>
-
 					<div className="row m-2">
 						<div className="col-12 text-center">
 							<i className="fas fa-paw fa-3x" />
@@ -26,15 +20,44 @@ export class AddForm extends React.Component {
 						</div>
 					</div>
 					<div className="col-12 mb-5 pb-3">
-						<input type="text" className="form-control mb-2 mt-4" placeholder="Nombre" />
-						<input type="text" className="form-control mb-2" placeholder="Lugar" />
-						<input type="text" className="form-control mb-2" placeholder="Identificación" />
+						<input type="text" className="form-control mb-2 mt-4" placeholder="Nombre mascota perdida" />
+						{/*<input type="text" className="form-control mb-2" placeholder="Lugar" /> no es necesario este dato lo arrojara la sollicitud misma*/}
+						<input type="text" className="form-control mb-2" placeholder="Número de Chip" />
 						<input type="text" className="form-control mb-2" placeholder="Raza" />
-						<input type="text" className="form-control mb-2" placeholder="Tamaño" />
-						<input type="text" className="form-control mb-2" placeholder="Color" />
-						<input type="text" className="form-control mb-2" placeholder="Sexo" />
+						<div className="row" role="group" aria-label="Basic example">
+							<div className="col-4 col-md-4">
+								<button type="button" className="btn btn-block commonBtn w-60 h-100 ">
+									<i className="fas fa-dog fa-2x black" />
+								</button>
+							</div>
+							<div className="col-4 col-md-4">
+								<button type="button" className="btn btn-block commonBtn w-60 h-100 ">
+									<i className="fas fa-dog fa-3x black" />
+								</button>
+							</div>
+							<div className="col-4 col-md-4">
+								<button type="button" className="btn btn-block commonBtn w-60 h-100 ">
+									<i className="fas fa-dog fa-4x black" />
+								</button>
+							</div>
+						</div>
+						{/*<input type="text" className="form-control mb-2" placeholder="Color" /> Esto esta demas s hay foto*/}
+						<div className="row mt-2" role="group" aria-label="Basic example">
+							<div className="col-2 col-md-2" />
+							<div className="col-4 col-md-4">
+								<button type="button" name="" className="btn btn-block commonBtn w-60 h-100">
+									<i className="fas fa-mars fa-3x black" />
+								</button>
+							</div>
+							<div className="col-4 col-md-4">
+								<button type="button" name="" className="btn btn-block commonBtn w-60 h-100">
+									<i className="fas fa-venus fa-3x black" />
+								</button>
+							</div>
+							<div className="col-2 col-md-2" />
+						</div>
 						<Link to="/Coincidences" className="text-center">
-							<button className="btn my-4 btn-block">Buscar</button>
+							<button className="btn my-4 btn-block importantBtn">Buscar</button>
 						</Link>
 					</div>
 				</div>
