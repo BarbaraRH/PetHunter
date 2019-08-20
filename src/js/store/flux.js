3deb5d5f26736fe8b5866e874d668adfb3b6f402
pablochile2017 <pablochile2017@gmail.com>
2019-08-20 23:51:23 +0000

    ultimo ajuste

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,7 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			apiServer: "https://3000-d609fce2-6fb7-4bd2-8d66-7edbc228308f.ws-us0.gitpod.io",
+			apiServer: "https://3000-a029fa6d-f544-403f-b6a3-ad42696ed441.ws-us0.gitpod.io",
 			cssStyleIconFooter: "",
 			selectChoice: "",
 			city: "",
@@ -125,34 +125,58 @@ const getState = ({ getStore, setStore }) => {
 					.catch(error => console.log(error));
 			},
 			submitAdvert: event => {
-				console.log("funciona");
+				{
+					/*inicio subir la foto------------------------------------------*/
+				}
 				event.preventDefault();
-				const store = getStore();
-				const inputs = event.target.getElementsByTagName("input");
-				fetch(store.apiServer + "/adverts", {
+				const form = event.target;
+				const f = form.file.files[0];
+				const data = new FormData();
+				data.append("file", f);
+				console.log(data);
+				const storea = getStore();
+				fetch(storea.apiServer + "/upload", {
 					method: "POST",
-					body: JSON.stringify({
-						name: inputs.petName.value,
-						chip_num: inputs.chip.value,
-						breed: store.breed,
-						size: store.size,
-						gender: store.gender,
-						status: store.statusName,
-						city: store.city,
-						district: store.district,
-						street1: inputs.street1.value,
-						street2: inputs.street2.value,
-						photo_url: store.apiServer + "/image/" + store.photoId
-					}),
-					headers: {
-						"Content-type": "application/json; charset=UTF-8"
-					}
+					body: data
 				})
 					.then(resp => resp.json())
 					.then(data => {
-						console.log(data);
+						setStore((storea.photoId = data.id));
+						const store = getStore();
+						const inputs = event.target.getElementsByTagName("input");
+						return fetch(store.apiServer + "/adverts", {
+							method: "POST",
+							body: JSON.stringify({
+								name: inputs.petName.value,
+								chip_num: inputs.chip.value,
+								breed: store.breed,
+								size: store.size,
+								gender: store.gender,
+								status: store.statusName,
+								city: store.city,
+								district: store.district,
+								street1: inputs.street1.value,
+								street2: inputs.street2.value,
+								photo_url: store.apiServer + "/image/" + store.photoId
+							}),
+							headers: {
+								"Content-type": "application/json; charset=UTF-8"
+							}
+						})
+							.then(resp => resp.json())
+							.then(data => {
+								console.log(data);
+							})
+							.catch(error => console.log(error));
 					})
 					.catch(error => console.log(error));
+				{
+					/*termino subir la foto------------------------------------------*/
+				}
+				{
+					/*inicio submitadvert------------------------------------------*/
+				}
+				console.log("funciona");
 			},
 			search: (status, event) => {
 				event.preventDefault();
@@ -190,24 +214,30 @@ const getState = ({ getStore, setStore }) => {
 				const store = getStore();
 				setStore((store[atr] = val));
 				console.log("ha sido guardado en el store." + atr + " : " + store[atr]);
-			},
-			submitFile: event => {
-				event.preventDefault();
-				const form = event.target;
-				const f = form.file.files[0];
-				const data = new FormData();
-				data.append("file", f);
-				console.log(data);
-				const store = getStore();
-				fetch(store.apiServer + "/upload", {
-					method: "POST",
-					body: data
+			}
+		},
+		upload: event => {
+			{
+				/*inicio subir la foto------------------------------------------*/
+			}
+			event.preventDefault();
+			const form = event.target;
+			const f = form.file.files[0];
+			const data = new FormData();
+			data.append("file", f);
+			console.log(data);
+			const store = getStore();
+			fetch(store.apiServer + "/upload", {
+				method: "POST",
+				body: data
+			})
+				.then(resp => resp.json())
+				.then(data => {
+					setStore((store.photoId = data.id));
 				})
-					.then(resp => resp.json())
-					.then(data => {
-						setStore((store.photoId = data.id));
-					})
-					.catch(error => console.log(error));
+				.catch(error => console.log(error));
+			{
+				/*termino subir la foto------------------------------------------*/
 			}
 		}
 	};
--- src/js/views/addForm.js
+++ src/js/views/addForm.js
@@ -15,135 +15,144 @@ export class AddForm extends React.Component {
 					<div className="row pt-5">
 						<div className="col-12 text-center mt-5">
 							<h2 className="mb-3">Datos de mascota perdida</h2>
-							<i className="fas fa-paw fa-3x pt-3" />
-						</div>
-						<div className="col-12 text-center">
-							<p>cargar foto</p>
+							<label htmlFor="file-input">
+								<div className="btn btn-outline-info">
+									<i className="fas fa-paw fa-3x pt-3" />
+									<h4>Click cargar foto</h4>
+								</div>
+							</label>
 						</div>
 					</div>
 
 					<Context.Consumer>
 						{({ store, actions }) => {
 							return (
-								<div className="col-12 mb-5 pb-3">
-									<form name="upload" onSubmit={() => actions.submitFile(event)}>
-										<input type="file" name="file" />
-										<button type="submit">Upload</button>
-									</form>
-									<form onSubmit={() => actions.submitAdvert(event)}>
-										<input
-											type="text"
-											name="petName"
-											className="form-control mb-2 mt-4"
-											placeholder="Nombre mascota"
-										/>
-
-										<input
-											type="number"
-											name="chip"
-											className="form-control mb-2"
-											placeholder="Número de Chip"
-										/>
-
-										{/*<input type="text" className="form-control mb-2" placeholder="Lugar" /> no es necesario este dato lo arrojara la sollicitud misma*/}
-
-										<div className="form-group">
-											<select
-												name="breed1"
-												value={store.breed}
-												className="form-control"
-												onChange={() => actions.handleSelect(event, "breed")}>
-												<option value="">Selecciona la Raza</option>
-												<option value="">A</option>
-												<option value="Alano">Alano</option>
-												<option value="Alaskan Malamute">Alaskan Malamute</option>
-												<option value="American Staffordshire Terrier">
-													American Staffordshire Terrier
-												</option>
-											</select>
-										</div>
-										<div className="form-group border-secondary p-4 rounded">
-											<div className="row">
-												<div className="col-12 text-center">
-													<p>Tamaño</p>
-													<select
-														name="size"
-														value={store.size}
-														className="form-control"
-														onChange={() => actions.handleSelect(event, "size")}>
-														<option value="">Selecciona el tamaño</option>
-														<option value="Chico">Chico</option>
-														<option value="Mediano">Mediano</option>
-														<option value="Grande">Grande</option>
-													</select>
-												</div>
-											</div>
-										</div>
-										<div className="form-group border-secondary p-4 rounded">
-											<div className="row ">
-												<div className="col-12 text-center">
-													<p>Género</p>
-												</div>
-											</div>
-
-											<div className="row">
-												<div className="col-12 text-center">
-													<select
-														name="gender"
-														value={store.gender}
-														className="form-control"
-														onChange={() => actions.handleSelect(event, "gender")}>
-														<option value="">Selecciona el genero</option>
-														<option value="macho">macho</option>
-														<option value="hembra">hembra</option>
-													</select>
-												</div>
-											</div>
-										</div>
-										<div className="form-group border-secondary p-4 rounded">
-											<p className="text-center mb-3">Lugar de extravío</p>
-											<select
-												className="form-control mb-2"
-												name="city"
-												value={store.city}
-												onChange={() => actions.handleSelect(event, "city")}>
-												<option value="">Elije tu Ciudad</option>
-												<option value="Santiago">Santiago</option>
-												<option value="Rancagua">Rancagua</option>
-												<option value="Valparaiso">Valparaiso</option>
-												<option value="Viña del Mar">Viña del Mar</option>
-											</select>
-											<select
-												className="form-control mb-2"
-												name="district"
-												value={store.district}
-												onChange={() => actions.handleSelect(event, "district")}>
-												<option value="">Elije tu comuna</option>
-												<option value="Santiago Centro">Santiago Centro</option>
-												<option value="Ñuñoa">Ñuñoa</option>
-												<option value="Providencia">Providencia</option>
-												<option value="Peñalolén">Peñalolén</option>
-											</select>
-											<p className="text-center mt-4 mb-3">Selecciona cruce de calles</p>
+								<div className="row">
+									<div className="col-12 mb-5 pb-3 text-center">
+										<form name="upload" onSubmit={() => actions.submitAdvert(event)}>
 											<input
-												type="text"
-												name="street1"
-												className="form-control mb-2"
-												placeholder="calle 1"
+												onClick={function() {
+													console.log("esto es");
+												}}
+												id="file-input"
+												type="file"
+												className="btn btn-info"
+												name="file"
 											/>
 											<input
 												type="text"
-												name="street2"
-												className="form-control mb-2"
-												placeholder="calle 2"
+												name="petName"
+												className="form-control mb-2 mt-4"
+												placeholder="Nombre mascota"
 											/>
-										</div>
-										<input
-											type="submit"
-											className="btn my-4 btn-block importantBtn"
-											placeholder="Buscar"
-										/>
-									</form>
+
+											<input
+												type="number"
+												name="chip"
+												className="form-control mb-2"
+												placeholder="Número de Chip"
+											/>
+
+											{/*<input type="text" className="form-control mb-2" placeholder="Lugar" /> no es necesario este dato lo arrojara la sollicitud misma*/}
+
+											<div className="form-group">
+												<select
+													name="breed1"
+													value={store.breed}
+													className="form-control"
+													onChange={() => actions.handleSelect(event, "breed")}>
+													<option value="">Selecciona la Raza</option>
+													<option value="">A</option>
+													<option value="Alano">Alano</option>
+													<option value="Alaskan Malamute">Alaskan Malamute</option>
+													<option value="American Staffordshire Terrier">
+														American Staffordshire Terrier
+													</option>
+												</select>
+											</div>
+											<div className="form-group border-secondary p-4 rounded">
+												<div className="row">
+													<div className="col-12 text-center">
+														<p>Tamaño</p>
+														<select
+															name="size"
+															value={store.size}
+															className="form-control"
+															onChange={() => actions.handleSelect(event, "size")}>
+															<option value="">Selecciona el tamaño</option>
+															<option value="Chico">Chico</option>
+															<option value="Mediano">Mediano</option>
+															<option value="Grande">Grande</option>
+														</select>
+													</div>
+												</div>
+											</div>
+											<div className="form-group border-secondary p-4 rounded">
+												<div className="row ">
+													<div className="col-12 text-center">
+														<p>Género</p>
+													</div>
+												</div>
+
+												<div className="row">
+													<div className="col-12 text-center">
+														<select
+															name="gender"
+															value={store.gender}
+															className="form-control"
+															onChange={() => actions.handleSelect(event, "gender")}>
+															<option value="">Selecciona el genero</option>
+															<option value="macho">macho</option>
+															<option value="hembra">hembra</option>
+														</select>
+													</div>
+												</div>
+											</div>
+											<div className="form-group border-secondary p-4 rounded">
+												<p className="text-center mb-3">Lugar de extravío</p>
+												<select
+													className="form-control mb-2"
+													name="city"
+													value={store.city}
+													onChange={() => actions.handleSelect(event, "city")}>
+													<option value="">Elije tu Ciudad</option>
+													<option value="Santiago">Santiago</option>
+													<option value="Rancagua">Rancagua</option>
+													<option value="Valparaiso">Valparaiso</option>
+													<option value="Viña del Mar">Viña del Mar</option>
+												</select>
+												<select
+													className="form-control mb-2"
+													name="district"
+													value={store.district}
+													onChange={() => actions.handleSelect(event, "district")}>
+													<option value="">Elije tu comuna</option>
+													<option value="Santiago Centro">Santiago Centro</option>
+													<option value="Ñuñoa">Ñuñoa</option>
+													<option value="Providencia">Providencia</option>
+													<option value="Peñalolén">Peñalolén</option>
+												</select>
+												<p className="text-center mt-4 mb-3">Selecciona cruce de calles</p>
+												<input
+													type="text"
+													name="street1"
+													className="form-control mb-2"
+													placeholder="calle 1"
+												/>
+												<input
+													type="text"
+													name="street2"
+													className="form-control mb-2"
+													placeholder="calle 2"
+												/>
+											</div>
+											<input
+												type="submit"
+												className="btn my-4 btn-block importantBtn"
+												placeholder="Buscar"
+											/>
+										</form>
+									</div>
 								</div>
 							);
 						}}
